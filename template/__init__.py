0831d2ff8190b3cf20ba03baa5b9b466a4bd4799
EC2 Default User <surcyf@gmail.com>
2023-11-19 06:10:27 +0000

    images

--- template/__init__.py
+++ template/__init__.py
@@ -19,7 +19,7 @@
 
 # TODO(developer): Change this value when updating your code base.
 # Define the version of the template module.
-__version__ = "0.0.0"
+__version__ = "1.1.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
