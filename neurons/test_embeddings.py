a42e85be16353fbf912ca5628a2c89dbd0e01fa5
EC2 Default User <surcyf@gmail.com>
2023-11-30 11:07:42 +0000

    embeddings

--- neurons/test_embeddings.py
+++ neurons/test_embeddings.py
@@ -14,39 +14,44 @@ if not AsyncOpenAI.api_key:
 client = AsyncOpenAI(timeout=30.0)
 
 async def embeddings(texts, model):
-    bt.logging.info(f"Received embeddings request: {texts, model}")
+    bt.logging.info(f"Received embeddings request for {len(texts)} texts with model {model}")
 
     async def get_embeddings_in_batch(texts, model, batch_size=10):
         batches = [texts[i:i + batch_size] for i in range(0, len(texts), batch_size)]
-        all_embeddings = []
+        tasks = []
         for batch in batches:
             filtered_batch = [text for text in batch if text.strip()]
-
             if filtered_batch:
-                response = await client.embeddings.create(input=filtered_batch, model=model)
-                batch_embeddings = [item.embedding for item in response.data]
-                all_embeddings.extend(batch_embeddings)
+                task = asyncio.create_task(client.embeddings.create(input=filtered_batch, model=model))
+                tasks.append(task)
             else:
                 bt.logging.info("Skipped an empty batch.")
+        
+        all_embeddings = []
+        for task in asyncio.as_completed(tasks):
+            try:
+                response = await task
+                batch_embeddings = [item.embedding for item in response.data]
+                all_embeddings.extend(batch_embeddings)
+            except Exception as e:
+                bt.logging.error(f"Error in processing batch: {e}")
+
         return all_embeddings
 
-
     try:
-        # Assuming 'client' is already defined and authenticated
         batched_embeddings = await get_embeddings_in_batch(texts, model=model)
         embeddings = [np.array(embed) for embed in batched_embeddings]
-
         return embeddings
     except Exception as e:
         bt.logging.error(f"Exception in embeddings function: {traceback.format_exc()}")
 
 
-def get_random_texts(dataset_name, config_name, num_samples=100):
+def get_random_texts(dataset_name, config_name, num_samples=10000):
     dataset = load_dataset(dataset_name, config_name)
     texts = [item['text'] for item in dataset['train']] 
     return random.sample(texts, num_samples)
 
-random_texts = get_random_texts('wikitext', 'wikitext-2-v1', 100)
+random_texts = get_random_texts('wikitext', 'wikitext-2-v1', 10000)
 print(random_texts)
 
 def main():
