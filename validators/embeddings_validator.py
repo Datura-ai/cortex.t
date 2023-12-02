c771c092fd4615f22c64cd8b1abd865fbc753ea4
EC2 Default User <surcyf@gmail.com>
2023-12-02 21:08:07 +0000

    major improvements + wandb correct logging

--- validators/embeddings_validator.py
+++ validators/embeddings_validator.py
@@ -19,6 +19,7 @@ class EmbeddingsValidator(BaseValidator):
         self.weight = 1
 
         self.wandb_data = {
+            "modality": "embeddings",
             "texts": {},
             "embeddings": {},
             "scores": {},
@@ -72,9 +73,6 @@ class EmbeddingsValidator(BaseValidator):
 
         query_responses = await asyncio.gather(*query_tasks)
         return query_responses, uid_to_question
-
-    async def handle_response(self, uid, responses):
-        return uid, responses
 
     async def score_responses(self, query_responses, uid_to_question):
         scores = torch.zeros(len(self.metagraph.hotkeys))
