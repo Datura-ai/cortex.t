6d1449076d9c31743b4343a17309f8b88d59192d
EC2 Default User <surcyf@gmail.com>
2023-12-03 05:49:51 +0000

    good extract_list version

--- template/utils.py
+++ template/utils.py
@@ -179,37 +179,42 @@ async def get_question(category, num_questions_needed):
 
 def preprocess_string(text):
     try:
-        processed_text = text.replace("\t", " ")
-
-        # Placeholder for single quotes within words
+        processed_text = text.replace("\t", "")
         placeholder = "___SINGLE_QUOTE___"
+        processed_text = re.sub(r"(?<=\w)'(?=\w)", placeholder, processed_text)
+        processed_text = processed_text.replace("'", '"').replace(placeholder, "'")
 
-        # Replace single quotes within words with the placeholder
-        processed_text = re.sub(r"(?<=\w)'(?=\w)", placeholder, text)
+        # Initialize variables to track whether we are inside quotes or a comment
+        inside_quotes = False
+        inside_comment = False
+        cleaned_text = []
 
-        # Replace single quotes used for enclosing strings with double quotes
-        processed_text = processed_text.replace("'", '"')
+        # Iterate through the text to remove spaces outside quotes and handle comments
+        for char in processed_text:
+            if char == '"':
+                inside_quotes = not inside_quotes
+                if inside_comment:
+                    inside_comment = False
+            elif char == '#' and not inside_quotes and not inside_comment:
+                inside_comment = True
+                continue
+            if not inside_quotes and char == ' ':
+                continue
+            if not inside_comment:
+                cleaned_text.append(char)
 
-        # Restore the original single quotes from the placeholder
-        processed_text = processed_text.replace(placeholder, "'")
+        cleaned_str = ''.join(cleaned_text)
+        cleaned_str = re.sub(r"\[\s+", "[", cleaned_str)
+        cleaned_str = re.sub(r"\s+\]", "]", cleaned_str)
 
-        # Delete spaces after an opening bracket '['
-        processed_text = re.sub(r"\[\s+", "[", processed_text)
-
-        # Delete spaces before a closing bracket ']'
-        processed_text = re.sub(r"\s+\]", "]", processed_text)
-
-        # Remove characters before first '[' and after first ']'
-        start = processed_text.find('[')
-        end = processed_text.find(']')
-
+        # Extract the portion within the outermost square brackets
+        start, end = cleaned_str.find('['), cleaned_str.rfind(']')
         if start != -1 and end != -1 and end > start:
-            processed_text = processed_text[start:end + 1]
+            cleaned_str = cleaned_str[start:end + 1]
 
-        return processed_text
-
+        return cleaned_str
     except Exception as e:
-        bt.logging.error(f"Error in preprocessing string: {traceback.format_exc()}")
+        print(f"Error in preprocessing string: {e}")
         return text
 
 def convert_to_list(text):
@@ -217,24 +222,27 @@ def convert_to_list(text):
     items = [item.strip() for item in re.split(pattern, text) if item]
     return items
 
-
 def extract_python_list(text: str):
     try:
         if re.match(r'\d+\.\s', text):
             return convert_to_list(text)
-        bt.logging.info(f"Preprocessed text = {text}")
+        
+        print(f"Preprocessed text = {text}")
         text = preprocess_string(text)
-        bt.logging.info(f"Postprocessed text = {text}")
-        match = re.search(r'\[((?:[^][]|"(?:\\.|[^"\\])*")*)\]', text)
-        if match:
-            list_str = match.group()
+        print(f"Postprocessed text = {text}")
 
-            evaluated = ast.literal_eval(list_str)
+        # Extracting list enclosed in square brackets
+        match = re.search(r'\[((?:[^][]|"(?:\\.|[^"\\])*")*)\]', text, re.DOTALL)
+        if match:
+            list_str = match.group(1)
+
+            # Using ast.literal_eval to safely evaluate the string as a list
+            evaluated = ast.literal_eval('[' + list_str + ']')
             if isinstance(evaluated, list):
                 return evaluated
 
     except Exception as e:
-        bt.logging.error(f"Unexpected error when extracting list: {e}\n{traceback.format_exc()}")
+        print(f"Unexpected error when extracting list: {e}\n{traceback.format_exc()}")
 
     return text
 
