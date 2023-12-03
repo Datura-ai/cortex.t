0bf7b1057b71179b6052c3011f7c4869e4741dce
EC2 Default User <surcyf@gmail.com>
2023-12-03 05:20:30 +0000

    more robust list extracting

--- tests/test_extract_list.py
+++ tests/test_extract_list.py
@@ -6,37 +6,42 @@ import traceback
 
 def preprocess_string(text):
     try:
-        processed_text = text.replace("\t", " ")
-
-        # Placeholder for single quotes within words
+        processed_text = text.replace("\t", "")
         placeholder = "___SINGLE_QUOTE___"
+        processed_text = re.sub(r"(?<=\w)'(?=\w)", placeholder, processed_text)
+        processed_text = processed_text.replace("'", '"').replace(placeholder, "'")
 
-        # Replace single quotes within words with the placeholder
-        processed_text = re.sub(r"(?<=\w)'(?=\w)", placeholder, text)
+        # Initialize variables to track whether we are inside quotes or a comment
+        inside_quotes = False
+        inside_comment = False
+        cleaned_text = []
 
-        # Replace single quotes used for enclosing strings with double quotes
-        processed_text = processed_text.replace("'", '"')
+        # Iterate through the text to remove spaces outside quotes and handle comments
+        for char in processed_text:
+            if char == '"':
+                inside_quotes = not inside_quotes
+                if inside_comment:
+                    inside_comment = False
+            elif char == '#' and not inside_quotes and not inside_comment:
+                inside_comment = True
+                continue
+            if not inside_quotes and char == ' ':
+                continue
+            if not inside_comment:
+                cleaned_text.append(char)
 
-        # Restore the original single quotes from the placeholder
-        processed_text = processed_text.replace(placeholder, "'")
+        cleaned_str = ''.join(cleaned_text)
+        cleaned_str = re.sub(r"\[\s+", "[", cleaned_str)
+        cleaned_str = re.sub(r"\s+\]", "]", cleaned_str)
 
-        # Delete spaces after an opening bracket '['
-        processed_text = re.sub(r"\[\s+", "[", processed_text)
-
-        # Delete spaces before a closing bracket ']'
-        processed_text = re.sub(r"\s+\]", "]", processed_text)
-
-        # Remove characters before first '[' and after first ']'
-        start = processed_text.find('[')
-        end = processed_text.find(']')
-
+        # Extract the portion within the outermost square brackets
+        start, end = cleaned_str.find('['), cleaned_str.rfind(']')
         if start != -1 and end != -1 and end > start:
-            processed_text = processed_text[start:end + 1]
+            cleaned_str = cleaned_str[start:end + 1]
 
-        return processed_text
-
+        return cleaned_str
     except Exception as e:
-        print(f"Error in preprocessing string: {traceback.format_exc()}")
+        print(f"Error in preprocessing string: {e}")
         return text
 
 def convert_to_list(text):
@@ -44,19 +49,22 @@ def convert_to_list(text):
     items = [item.strip() for item in re.split(pattern, text) if item]
     return items
 
-
 def extract_python_list(text: str):
     try:
         if re.match(r'\d+\.\s', text):
             return convert_to_list(text)
+        
         print(f"Preprocessed text = {text}")
         text = preprocess_string(text)
         print(f"Postprocessed text = {text}")
-        match = re.search(r'\[((?:[^][]|"(?:\\.|[^"\\])*")*)\]', text)
-        if match:
-            list_str = match.group()
 
-            evaluated = ast.literal_eval(list_str)
+        # Extracting list enclosed in square brackets
+        match = re.search(r'\[((?:[^][]|"(?:\\.|[^"\\])*")*)\]', text, re.DOTALL)
+        if match:
+            list_str = match.group(1)
+
+            # Using ast.literal_eval to safely evaluate the string as a list
+            evaluated = ast.literal_eval('[' + list_str + ']')
             if isinstance(evaluated, list):
                 return evaluated
 
@@ -67,14 +75,14 @@ def extract_python_list(text: str):
 
 
 text1 = """
-```python entertainment_complex_questions = [     # Question 1     "Develop a comprehensive algorithm to predict the box office success of movies across different genres and global markets, considering variables such as cast popularity, marketing budget, release timing, and historical data of similar movies.",          # Question 2     "Create a neural network model that can generate original scripts for a TV series that match the linguistic style and thematic depth of a given showrunner, such as Aaron Sorkin or Shonda Rhimes, and test its effectiveness by comparing it with human-written scripts.",          # Question 3     "Design a virtual reality experience that integrates live performances with interactive audience participation, ensuring the experience is adaptable to multiple entertainment genres including concerts, theater, and sport events." ] ```
+python entertainment_complex_questions = [     # Question 1     "Develop a comprehensive algorithm to predict the box office success of movies across different genres and global markets, considering variables such as cast popularity, marketing budget, release timing, and historical data of similar movies.",          # Question 2     "Create a neural network model that can generate original scripts for a TV series that match the linguistic style and thematic depth of a given showrunner, such as Aaron Sorkin or Shonda Rhimes, and test its effectiveness by comparing it with human-written scripts.",          # Question 3     "Design a virtual reality experience that integrates live performances with interactive audience participation, ensuring the experience is adaptable to multiple entertainment genres including concerts, theater, and sport events." ] 
 """
 text2 = """
- ```python economy_related_tasks = [     "Write a program that simulates the impact of a small tax cut on a simplified supply-demand model, considering only three industries, with relevance to broader economic factors being minimal.",     "Create a Monte Carlo simulation to project the potential variations in a local market's commodity prices, with little consideration for macroeconomic indicators such as GDP or employment rates.",     "Develop a script that scrapes data from a social media platform to gauge consumer sentiment on a new tech gadget, ignoring the broader economic implications of consumer electronics on the economy.",     "Implement an algorithm that predicts the stock price of a fictional company based on random fluctuations, without any connection to economic indicators like inflation or interest rates." ] ```
-"""
+ python economy_related_tasks = [     "Write a program that simulates the impact of a small tax cut on a simplified supply-demand model, considering only three industries, with relevance to broader economic factors being minimal.",     "Create a Monte Carlo simulation to project the potential variations in a local market's commodity prices, with little consideration for macroeconomic indicators such as GDP or employment rates.",     "Develop a script that scrapes data from a social media platform to gauge consumer sentiment on a new tech gadget, ignoring the broader economic implications of consumer electronics on the economy.",     "Implement an algorithm that predicts the stock price of a fictional company based on random fluctuations, without any connection to economic indicators like inflation or interest rates." ] 
+ """
 text3 = """
 
 """
 
-extract_python_list(text1)
+extract_python_list(text2)
 
