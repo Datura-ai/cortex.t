ad19dbe11d9f410712b1f3a2dc3beb33baec3f4b
surcyf123 <114649324+surcyf123@users.noreply.github.com>
2024-01-29 12:38:34 -0500

    improve text models

--- validators/text_validator.py
+++ validators/text_validator.py
@@ -16,7 +16,7 @@ class TextValidator(BaseValidator):
         super().__init__(dendrite, config, subtensor, wallet, timeout=60)
         self.streaming = True
         self.query_type = "text"
-        self.model =  "gpt-3.5-turbo" # "gpt-4-1106-preview"
+        self.model =  "gpt-4-1106-preview"
         self.max_tokens = 2048
         self.temperature = 0.0001
         self.weight = 1
@@ -81,9 +81,9 @@ class TextValidator(BaseValidator):
         if self.provider == "Anthropic":
             # bedrock models = ["anthropic.claude-v2:1", "anthropic.claude-instant-v1", "anthropic.claude-v1", "anthropic.claude-v2"]
             # claude models = ["claude-2.1", "claude-2.0", "claude-instant-1.2"]
-            self.model = "anthropic.claude-instant-v1"
+            self.model = "anthropic.claude-v2:1"
         elif self.provider == "OpenAI":
-            self.model = "gpt-3.5-turbo"
+            self.model = "gpt-4-1106-preview"
 
         for uid in available_uids:
             prompt = await self.get_question(len(available_uids))
