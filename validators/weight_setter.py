d5f8d99fbdbb28abaddfa957883826c6b5f72712
surcyf123 <114649324+surcyf123@users.noreply.github.com>
2024-01-30 12:26:57 -0500

    revert wandb deletion

--- validators/weight_setter.py
+++ validators/weight_setter.py
@@ -75,22 +75,6 @@ class WeightSetter:
             except Exception as e:
                 bt.logging.error(f'Encountered in {self.consume_organic_scoring.__name__} loop:\n{traceback.format_exc()}')
                 await asyncio.sleep(10)
-
-    def remove_directory(self, path):
-        # Remove wandb logs
-        try:
-            # Expands the '~' to the home directory
-            full_path = os.path.expanduser(path)
-        
-            # Check if the directory exists
-            if os.path.exists(full_path):
-                # Recursively delete the directory and its contents
-                shutil.rmtree(full_path)
-                print(f"Successfully removed {full_path}")
-            else:
-                print(f"The directory {full_path} does not exist.")
-        except Exception as e:
-            bt.logging.error(f"error in remove directory {traceback.format_exc()}")
                 
             
     async def perform_synthetic_scoring_and_update_weights(self):
@@ -106,7 +90,6 @@ class WeightSetter:
                 steps_since_last_update = steps_passed % iterations_per_set_weights
 
                 if steps_since_last_update == iterations_per_set_weights - 1:
-                    self.remove_directory('~/.bittensor/miners/')
                     await self.update_weights(steps_passed)
                 else:
                     bt.logging.info(
