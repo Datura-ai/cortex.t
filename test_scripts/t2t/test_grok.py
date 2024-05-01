3b7cfe951d8878d1ce46969826ccb8ba0d58f72e
doge <surcyf@gmail.com>
2024-05-01 18:38:33 +0000

    groq working

---
+++ test_scripts/t2t/test_grok.py
@@ -0,0 +1,19 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="mixtral-8x7b-32768",
+)
+
+print(chat_completion.choices[0].message.content)
