1b7c12c846f9735ae20594c059e824d7b21453e5
i-beck <99846875+i-beck@users.noreply.github.com>
2024-07-18 13:32:13 -0400

    added docstrings

--- miner/config.py
+++ miner/config.py
@@ -112,12 +112,27 @@ def get_config() -> bt.config:
 
 
 def check_endpoint_overrides() -> bool:
+    """Check if the endpoint_overrides.yaml file exists in the current directory or script directory.
+
+    Returns:
+        bool: True if the file exists in either location, False otherwise."""
+
     file_path = path.join(path.dirname(__file__), "endpoint_overrides.yaml")
     script_dir_file_path = path.join(path.dirname(path.abspath(argv[0])), "endpoint_overrides.yaml")
     return path.exists(file_path) or path.exists(script_dir_file_path)
 
 
 def get_endpoint_overrides() -> dict:
+    """Get endpoint overrides from the endpoint_overrides.yaml file.
+
+    Returns:
+        dict: A dictionary containing the endpoint overrides.
+
+    Raises:
+        FileNotFoundError: If the endpoint_overrides.yaml file is not found.
+        Exception: If an error occurs while reading the file.
+    """
+
     if not check_endpoint_overrides():
         return {}
     try:
@@ -131,4 +146,12 @@ def get_endpoint_overrides() -> dict:
 
 
 def override_endpoint_keys() -> None:
+    """Override endpoint keys based on the values provided in the endpoint_overrides.yaml file.
+
+    Args:
+        None
+
+    Returns:
+        None"""
+
     environ.update(get_endpoint_overrides().get("OVERRIDE_ENVIRONMENT_KEYS", {}))
