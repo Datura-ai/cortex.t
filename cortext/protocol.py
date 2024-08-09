161d2f688eccbefd9f12c96d6892c437456de473
acer-king <acerking03@gmail.com>
2024-08-09 01:26:30 -0700

    feature: code-refactoring update Embedding model

--- cortext/protocol.py
+++ cortext/protocol.py
@@ -129,7 +129,11 @@ class ImageResponse(bt.Synapse):
 
 class Embeddings(bt.Synapse):
     """ A class to represent the embeddings request and response. """
-    provider: str = "Embedding"
+    provider: str = pydantic.Field(
+        default="OpenAI",
+        title="text",
+        description="Provider name by which embeddings are to be generated"
+    )
 
     texts: List[str] = pydantic.Field(
         ...,
