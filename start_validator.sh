c4bb5df03153a6a4ccc1e1f22a384b6e690b402c
acer-king <acerking03@gmail.com>
2024-08-19 10:07:34 -0700

    feature: code-refactoring added start_miner.sh start_validator.sh

--- start_validator.sh
+++ start_validator.sh
@@ -1,1 +1,16 @@
-python3 -m validators.validator --subtensor.network test --netuid 196 --wallet.name miner --wallet.hotkey default
+#!/bin/bash
+
+read -p "Enter the subtensor.network argument [test]: " arg1
+arg1=${arg1:-test }
+
+read -p "Enter the netuid argument [24]: " arg2
+arg2=${arg2:-24}
+
+read -p "Enter the wallet.name argument [validator]: " arg3
+arg3=${arg3:-validator}
+
+read -p "Enter the wallet.hotkey argument [default]: " arg4
+arg4=${arg4:-default}
+
+# Run the Python script with the provided arguments
+python3 -m validators.validator --subtensor.network $arg1 --netuid $arg2 --wallet.name $arg3 --wallet.hotkey $arg4
