7ebb669de5cc72cf10c7b8e30c811a2dc3a8d952
acer-king <acerking03@gmail.com>
2024-08-20 01:21:05 -0700

    feature: code-refactoring update start_validator.sh start_miner.sh to enable wandb_off tracing

--- start_miner.sh
+++ start_miner.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-read -p "Enter the subtensor.network argument [test]: " arg1
-arg1=${arg1:-test }
+read -p "Enter the subtensor.network argument [test]: " network
+network=${network:-test }
 
-read -p "Enter the netuid argument [24]: " arg2
-arg2=${arg2:-24}
+read -p "Enter the netuid argument [24]: " netuid
+netuid=${netuid:-24}
 
-read -p "Enter the wallet.name argument [validator]: " arg3
-arg3=${arg3:-validator}
+read -p "Enter the wallet.name argument [validator]: " wallet_name
+wallet_name=${wallet_name:-validator}
 
-read -p "Enter the wallet.hotkey argument [default]: " arg4
-arg4=${arg4:-default}
+read -p "Enter the wallet.hotkey argument [default]: " wallet_hotkey
+wallet_hotkey=${wallet_hotkey:-default}
+
+read -p "you want disable wandb [true]: " wandb_off
+wandb_off=${wandb_off:-true}
+
+read -p "you want enable tracing [true]: " tracing
+tracing=${tracing:-true}
+
+command_to_run="python3 -m miner.miner --subtensor.network $network --netuid $netuid --wallet.name $wallet_name --wallet.hotkey $wallet_hotkey"
+
+if [ "$tracing" = "true" ]; then
+    command_to_run="$command_to_run --logging.trace"
+fi
+
+if [ "$wandb_off" = "true" ]; then
+    command_to_run="$command_to_run --wandb_off"
+fi
 
 # Run the Python script with the provided arguments
-python3 -m miner.miner --subtensor.network $arg1 --netuid $arg2 --wallet.name $arg3 --wallet.hotkey $arg4
+echo $command_to_run
+$command_to_run "$@"
