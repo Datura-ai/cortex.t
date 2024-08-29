a1616f981f776a5597a9acee45aefad854924722
acer-king <acerking03@gmail.com>
2024-08-29 00:52:02 -0700

    refactoring: added auto-update

--- start_validator.py
+++ start_validator.py
@@ -1,21 +1,33 @@
 import argparse
-import os
+import time
 import subprocess
-from dotenv import load_dotenv
+import cortext
+from cortext.utils import get_version, send_discord_alert
 
-load_dotenv()  # Load environment variables from .env file
-env_ = str(os.getenv('ENV')).upper()
-default_address = "wss://test.finney.opentensor.ai:443" if env_ == 'DEV' \
-    else "wss://entrypoint-finney.opentensor.ai:443"
-subtensor_network = 'test' if env_ == 'DEV' else 'local'
-net_uid = '196' if env_ == 'DEV' else '18'
+default_address = "wss://bittensor-finney.api.onfinality.io/public-ws"
+webhook_url = ""
+current_version = cortext.__version__
 
+def update_and_restart(pm2_name, wallet_name, wallet_hotkey, address, autoupdate):
+    global current_version
+    subprocess.run(["pm2", "start", "--name", pm2_name, "python3 -m validators.validator", "--", "--wallet.name", wallet_name, "--wallet.hotkey", wallet_hotkey, "--netuid", "18", "--subtensor.network", "local", "--subtensor.chain_endpoint", address])
+    while True:
+        latest_version = get_version()
+        print(f"Current version: {current_version}")
+        print(f"Latest version: {latest_version}")
 
-def update_and_restart(wallet_name, wallet_hotkey, address, autoupdate):
-    subprocess.run(["python3", "-m", "validators.validator",
-                    "--wallet.name", wallet_name, "--wallet.hotkey", wallet_hotkey, "--netuid", net_uid,
-                    "--subtensor.network", subtensor_network, "--subtensor.chain_endpoint", address])
-
+        if current_version != latest_version and latest_version != None:
+            if not autoupdate:
+                send_discord_alert(f"Your validator not running the latest code ({current_version}). You will quickly lose vturst if you don't update to version {latest_version}", webhook_url)
+            print("Updating to the latest version...")
+            subprocess.run(["pm2", "delete", pm2_name])
+            subprocess.run(["git", "reset", "--hard"])
+            subprocess.run(["git", "pull"])
+            subprocess.run(["pip", "install", "-e", "."])
+            subprocess.run(["pm2", "start",  "--name", pm2_name, "python3 -m validators.validator", "--name", pm2_name, "--", "--wallet.name", wallet_name, "--wallet.hotkey", wallet_hotkey, "--netuid", "18", "--subtensor.network", "local", "--subtensor.chain_endpoint", address])
+            current_version = latest_version
+        print("All up to date!")
+        time.sleep(180)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -23,16 +35,15 @@ if __name__ == "__main__":
         epilog="Example usage: python start_validator.py --pm2_name 'net18vali' --wallet_name 'wallet1' --wallet_hotkey 'key123' [--address 'wss://...'] [--no-autoupdate]"
     )
 
+    parser.add_argument("--pm2_name", required=True, help="Name of the PM2 process.")
     parser.add_argument("--wallet_name", required=True, help="Name of the wallet.")
     parser.add_argument("--wallet_hotkey", required=True, help="Hotkey for the wallet.")
-    parser.add_argument("--address", default=default_address,
-                        help="Subtensor chain_endpoint, defaults to 'wss://bittensor-finney.api.onfinality.io/public-ws' if not provided.")
-    parser.add_argument("--no-autoupdate", action='store_false', dest='autoupdate',
-                        help="Disable automatic update. Only send a Discord alert. Add your webhook at the top of the script.")
+    parser.add_argument("--address", default=default_address, help="Subtensor chain_endpoint, defaults to 'wss://bittensor-finney.api.onfinality.io/public-ws' if not provided.")
+    parser.add_argument("--no-autoupdate", action='store_false', dest='autoupdate', help="Disable automatic update. Only send a Discord alert. Add your webhook at the top of the script.")
 
     args = parser.parse_args()
 
     try:
-        update_and_restart(args.wallet_name, args.wallet_hotkey, args.address, args.autoupdate)
+        update_and_restart(args.pm2_name, args.wallet_name, args.wallet_hotkey, args.address, args.autoupdate)
     except Exception as e:
         parser.error(f"An error occurred: {e}")
