b66ead0200e404fbc5a5706e7bb9a674a82f6c39
Ubuntu <ubuntu@ip-172-31-75-184.ec2.internal>
2024-09-02 19:29:01 +0000

    decrease probability to score

--- validators/services/validators/text_validator.py
+++ validators/services/validators/text_validator.py
@@ -148,7 +148,7 @@ class TextValidator(BaseValidator):
 
     def should_i_score(self):
         random_number = random.random()
-        will_score_all = random_number < 1 / 1
+        will_score_all = random_number < 1 / 5
         bt.logging.info(f"Random Number: {random_number}, Will score text responses: {will_score_all}")
         return will_score_all
 
