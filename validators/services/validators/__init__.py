8b47e191ab0f5d38ab5f1fbaad683b223961406d
Ubuntu <ubuntu@ip-172-31-75-184.ec2.internal>
2024-09-13 08:28:42 +0000

    add back text validator to init

--- validators/services/validators/__init__.py
+++ validators/services/validators/__init__.py
@@ -1,3 +1,4 @@
 from .image_validator import ImageValidator
 from .embeddings_validator import EmbeddingsValidator
 from .base_validator import BaseValidator
+from .text_validator import TextValidator
