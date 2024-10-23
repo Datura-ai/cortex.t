9697ad30e618e1eb1f6a39d2b8973b4c6e56a097
acer-king <acerking03@gmail.com>
2024-10-23 02:28:24 -0700

    apply timeout for individual request

--- cortext/dendrite.py
+++ cortext/dendrite.py
@@ -50,7 +50,6 @@ class CortexDendrite(dendrite):
         try:
             while max_try < 3:
                 if not session:
-                    timeout = aiohttp.ClientTimeout(total=60, connect=timeout, sock_connect=timeout, sock_read=timeout)
                     connector = aiohttp.TCPConnector(limit=200)
                     session = aiohttp.ClientSession(timeout=timeout, connector=connector)
                     CortexDendrite.miner_to_session[endpoint] = session
@@ -58,6 +57,7 @@ class CortexDendrite(dendrite):
                         url,
                         headers=synapse.to_headers(),
                         json=synapse.dict(),
+                        timeout = aiohttp.ClientTimeout(total=timeout)
                 ) as response:
                     # Use synapse subclass' process_streaming_response method to yield the response chunks
                     try:
