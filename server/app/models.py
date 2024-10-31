bc30eb052a508c46e25e2145825a4144600672d2
acer-king <acerking03@gmail.com>
2024-10-31 06:26:30 -0700

    fix issue in central server model

--- server/app/models.py
+++ server/app/models.py
@@ -4,15 +4,15 @@ from pydantic import BaseModel, Field
 
 class Filters(BaseModel):
     min_score: int = Field(0, ge=0)
-    min_similarity: int = Field(120, ge=0)
+    min_similarity: int = Field(0.0, ge=0)
     model: Optional[str] = Field(None)
     provider: Optional[str] = Field(None)
-    min_timestamp: Optional[int] = Field(None, ge=0)
-    max_timestamp: Optional[int] = Field(None, ge=0)
+    min_timestamp: Optional[int] = Field(0, ge=0)
+    max_timestamp: Optional[int] = Field(999999999999, ge=0)
 
 
 class RequestBody(BaseModel):
     filters: Filters
     search: Union[int, str] = Field(..., description="An integer ID or a string search key")
     sort_by: str = Field("miner", description="Field to sort by")
-    sort_order: str = Field("desc", regex="^(asc|desc)$", description="Sorting order, 'asc' or 'desc'")
+    sort_order: str = Field("desc", pattern="^(asc|desc)$", description="Sorting order, 'asc' or 'desc'")
