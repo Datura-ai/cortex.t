614cff3a4ae9ce30919f0baea7552fc892c9ede4
acer-king <acerking03@gmail.com>
2024-11-05 08:54:47 -0800

    allow all origin cors in central server

--- server/app/main.py
+++ server/app/main.py
@@ -17,9 +17,6 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
 )
 
 
