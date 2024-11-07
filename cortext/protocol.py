cd44f46c577dda6ff3de22a72c976635d4ccfc9d
acer-king <acerking03@gmail.com>
2024-11-07 03:02:50 -0800

    increase buffer size for synthetic queries

--- cortext/protocol.py
+++ cortext/protocol.py
@@ -369,7 +369,7 @@ class StreamPrompting(bt.StreamingSynapse):
     async def process_streaming_response(self, response: StreamingResponse, organic=True) -> AsyncIterator[str]:
         if self.completion is None:
             self.completion = ""
-        chunk_size = 100 if organic else 1024
+        chunk_size = 100 if organic else 10000
         async for chunk in response.content.iter_chunked(chunk_size):
             tokens = chunk.decode("utf-8")
             self.completion += tokens
