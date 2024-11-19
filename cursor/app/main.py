b458a2760b5d49dd0e6ab34e0fadcdfe6efa2010
acer-king <acerking03@gmail.com>
2024-11-19 08:59:38 -0800

    cursor support

--- cursor/app/main.py
+++ cursor/app/main.py
@@ -1,19 +1,12 @@
-from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from . import curd, models, schemas
-from .database import create_table, conn, cur
 from typing import List
 from .endpoints.text import router as chat_router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Load the ML model
-    await create_table(None)
-    yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
