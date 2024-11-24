c49818a770c8c04ad0cb7486daf77e972e206f49
acer-king <acerking03@gmail.com>
2024-11-23 22:40:38 -0800

    cursor support

--- cursor/app/main.py
+++ cursor/app/main.py
@@ -1,10 +1,16 @@
-
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from .endpoints.text import router as chat_router
 
-
-
 app = FastAPI()
+# Add CORS middleware to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 app.include_router(chat_router)
