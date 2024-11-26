4e81c1515d9f8d9d77d15876615e35f8ec51e3ea
acer-king <acerking03@gmail.com>
2024-11-26 02:17:51 -0800

    cursor support

--- cursor/app/main.py
+++ cursor/app/main.py
@@ -14,6 +14,7 @@ class APIKeyMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         # Get the API key from the `Authorization` header
         api_key = request.headers.get("Authorization")
+        print("api_key is: ", api_key)
 
         # Validate the API key
         if not api_key or api_key not in VALID_API_KEYS:
