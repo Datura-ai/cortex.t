c3f1173774bf478ca4c297dbf22a498ba08a9bee
acer-king <acerking03@gmail.com>
2024-11-27 09:29:15 -0800

    cursor support

--- cursor/app/core/middleware.py
+++ cursor/app/core/middleware.py
@@ -1,7 +1,29 @@
-import time
-from fastapi import HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Request
+from cursor.app.core.config import config
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import JSONResponse
+# Your predefined valid API keys
+VALID_API_KEYS = {config.api_key}
 
 
-async def verify_api_key_rate_limit(config, api_key):
-    # NOTE: abit dangerous but very useful
-    pass
+class APIKeyMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        # Get the API key from the `Authorization` header
+        if request.method == "OPTIONS":
+            return await call_next(request)
+
+        if not request.headers.get("Authorization"):
+            return JSONResponse(
+                {"detail": "Invalid or missing API Key"}, status_code=401
+            )
+
+        api_key = request.headers.get("Authorization").split(" ")[1]
+
+        # Validate the API key
+        if not api_key or api_key not in VALID_API_KEYS:
+            return JSONResponse(
+                {"detail": "Invalid or missing API Key"}, status_code=401
+            )
+
+        # Proceed to the next middleware or route handler
+        return await call_next(request)
