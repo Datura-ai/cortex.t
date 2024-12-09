6b8103337f3759c41ea7dc74e75c932cda8e5bd9
acer-king <acerking03@gmail.com>
2024-12-09 03:34:28 -0800

    dendrite issue fix

--- cortext/dendrite.py
+++ cortext/dendrite.py
@@ -56,7 +56,7 @@ class CortexDendrite(dendrite):
                 async with session.post(
                         url,
                         headers=synapse.to_headers(),
-                        json=synapse.model_dump(),
+                        json=synapse.dict(),
                 ) as response:
                     # Use synapse subclass' process_streaming_response method to yield the response chunks
                     try:
