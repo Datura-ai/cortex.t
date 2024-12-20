73cbe8fd2d9e6e075d462946a3ec85d8ec410522
acer-king <acerking03@gmail.com>
2024-12-20 02:29:04 -0800

    update bittensor to 8.5.1

--- cursor/app/core/config.py
+++ cursor/app/core/config.py
@@ -20,12 +20,16 @@ class Config:
         parser = argparse.ArgumentParser(description="Bittensor example script")
         parser.add_argument('--wallet.name', type=str, default="default", help="wallet name")
         parser.add_argument('--wallet.hotkey', type=str, default="default", help="wallet hotkey")
+        parser.add_argument('--netuid', type=int, default=18, help="netuid")
+        parser.add_argument('--subtensor.chain_endpoint', type=str, default="finney", help="network name")
         bt_config = bt.config(parser)
         """Load configuration from environment variables."""
         return Config(
             wallet_name=bt_config.wallet.name,  # Default to an empty string if not set
             wallet_hotkey=bt_config.wallet.hotkey,
-            api_key=os.getenv("CURSOR_API_KEY", "")
+            api_key=os.getenv("CURSOR_API_KEY", ""),
+            netuid = bt_config.netuid,
+            network = "test" if "test" in bt_config.subtensor.chain_endpoint else "finney"
         )
 
 
